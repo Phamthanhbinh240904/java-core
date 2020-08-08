407287b232a1e3b531f95af934baf392d36228c4
BinhPT <phamthanhbinh2409iuh@gmail.com>
2020-08-08 15:55:06 +0700

    first commit

---
+++ src/buoi1/Buoi1.java
@@ -0,0 +1,14 @@
+package buoi1;
+
+import java.util.Scanner;
+
+public class Buoi1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("nhap n");
+        int n=sc.nextInt();
+        int a=n;
+        System.out.println("so n+10:"+(n+10));
+    }
+}
